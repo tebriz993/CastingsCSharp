38775531bfb8ff208485dff1e8521099a516d53e
Tabriz Latifov <130743534+tebriz993@users.noreply.github.com>
2023-12-01 09:24:58 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkCastingTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] basket = new string[] { "Apple", "Pinapple", "Orange" };
+            foreach (var fruits in basket)
+            {
+                Console.WriteLine(fruits);
+            }
+        }
+    }
+
+
+    abstract class Fruit
+    {
+        public int Price { get; set; }
+        public string Sort { get; set; }
+
+        public abstract void Taste();
+
+
+
+    }
+
+    class Apple : Fruit
+    {
+        public int vitaminA = 27;
+        public int vitaminB = 33;
+
+
+
+        public override void Taste()
+        {
+            Console.WriteLine();
+        }
+
+
+    }
+
+    class Pineapple : Fruit
+    {
+        public int vitaminE = 56;
+        public int vitaminD = 19;
+
+        public override void Taste()
+        {
+            Console.WriteLine();
+        }
+    }
+
+    class Orange : Fruit
+    {
+        public int vitaminC = 88;
+
+        public override void Taste()
+        {
+            Console.WriteLine();
+        }
+
+    }
+}
